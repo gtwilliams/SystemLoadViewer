9058775f4dfdb0c1a8da5dc25089c1e79aac9e4b
Garry T. Williams <gtwilliams@gmail.com>
2018-11-01 23:42:08 -0400

    Add patch commands to run on system.

---
+++ patch-this.sh
@@ -0,0 +1,3 @@
+sudo patch /usr/share/plasma/plasmoids/org.kde.plasma.systemloadviewer/contents/ui/SystemLoadViewer.qml ui/SystemLoadViewer.qml.patch
+sudo patch /usr/share/plasma/plasmoids/org.kde.plasma.systemloadviewer/contents/ui/ColorSettings.qml ui/ColorSettings.qml.patch
+sudo patch /usr/share/plasma/plasmoids/org.kde.plasma.systemloadviewer/contents/config/main.xml config/main.xml.patch
